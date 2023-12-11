8de8b8b2116da52900ecd56bbc4b6993a763a57c
Kirill <ynwa_2016@mail.ru>
2023-12-11 13:29:29 +0300

    new

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from scipy.stats import mode
+from sklearn.neighbors import KNeighborsClassifier
+
+df = pd.read_csv("source.csv")
 
 
-raw_data = pd.read_csv('source.csv')
-print(raw_data)
+
+print(df.head())
+
+
